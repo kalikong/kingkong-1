cf357fb703a46570af247fe1d2b8f4ea44d35d38
vembririyan <vembrivrd@gmail.com>
2020-02-29 20:35:51 +0700

    hhh

---
+++ app/src/main/java/com/ailadev/kingkong1/HomeJenius.java
@@ -0,0 +1,14 @@
+package com.ailadev.kingkong1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeJenius extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_jenius);
+    }
+}
---
+++ app/src/main/java/com/ailadev/kingkong1/InOut.java
@@ -0,0 +1,14 @@
+package com.ailadev.kingkong1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InOut extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_out);
+    }
+}
